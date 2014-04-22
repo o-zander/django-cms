aae860b4da6f45077f1ef342baac9e863c91e8cd
Oliver Zander <zander@cosmocode.de>
2014-04-22 12:03:30 +0200

    made user model configurable

--- cms/models/moderatormodels.py
+++ cms/models/moderatormodels.py
@@ -3,11 +3,13 @@ import sys
 from cms.utils.compat.dj import python_2_unicode_compatible
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 from cms.models.managers import PageModeratorStateManager
 from cms.models.pagemodel import Page
+
+user_model_label = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 ################################################################################
@@ -28,7 +30,7 @@ class PageModeratorState(models.Model):
     )
 
     page = models.ForeignKey(Page)
-    user = models.ForeignKey(User, null=True)
+    user = models.ForeignKey(user_model_label, null=True)
     created = models.DateTimeField(auto_now_add=True)
     action = models.CharField(max_length=3, choices=_action_choices, null=True, blank=True)
     message = models.TextField(max_length=1000, blank=True, default="")
